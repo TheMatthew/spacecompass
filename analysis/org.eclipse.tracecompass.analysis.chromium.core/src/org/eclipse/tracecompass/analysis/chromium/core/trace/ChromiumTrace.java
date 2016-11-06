c76105c1c194f5895b7edab946a9e6ceae167584
Matthew Khouzam <matthew.khouzam@gmail.com>
2016-11-06 01:39:49 -0400

    analysis: introduce Chromium Trace support
    Change-Id: I12b5f878eb2584eac6321e2f7c2e18a66f0b2707
    Signed-off-by: Matthew Khouzam <matthew.khouzam@gmail.com>

---
+++ analysis/org.eclipse.tracecompass.analysis.chromium.core/src/org/eclipse/tracecompass/analysis/chromium/core/trace/ChromiumTrace.java
@@ -0,0 +1,233 @@
+package org.eclipse.tracecompass.analysis.chromium.core.trace;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.util.Collections;
+import java.util.Map;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.tracecompass.internal.analysis.chromium.core.Activator;
+import org.eclipse.tracecompass.internal.analysis.chromium.core.event.ChromiumEvent;
+import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
+import org.eclipse.tracecompass.tmf.core.exceptions.TmfTraceException;
+import org.eclipse.tracecompass.tmf.core.io.BufferedRandomAccessFile;
+import org.eclipse.tracecompass.tmf.core.project.model.ITmfPropertiesProvider;
+import org.eclipse.tracecompass.tmf.core.trace.ITmfContext;
+import org.eclipse.tracecompass.tmf.core.trace.TmfContext;
+import org.eclipse.tracecompass.tmf.core.trace.TmfTrace;
+import org.eclipse.tracecompass.tmf.core.trace.TmfTraceManager;
+import org.eclipse.tracecompass.tmf.core.trace.TmfTraceUtils;
+import org.eclipse.tracecompass.tmf.core.trace.TraceValidationStatus;
+import org.eclipse.tracecompass.tmf.core.trace.indexer.ITmfPersistentlyIndexable;
+import org.eclipse.tracecompass.tmf.core.trace.location.ITmfLocation;
+import org.eclipse.tracecompass.tmf.core.trace.location.TmfLongLocation;
+
+/**
+ * @author matthew
+ *
+ */
+public class ChromiumTrace extends TmfTrace implements ITmfPersistentlyIndexable, ITmfPropertiesProvider {
+
+    private static final int ESTIMATED_EVENT_SIZE = 90;
+    private static final TmfLongLocation NULL_LOCATION = new TmfLongLocation(-1L);
+    private static final TmfContext INVALID_CONTEXT = new TmfContext(NULL_LOCATION, ITmfContext.UNKNOWN_RANK);
+    private static final int MAX_LINES = 100;
+    private static final int MAX_CONFIDENCE = 100;
+
+    private File fFile;
+
+    private RandomAccessFile fFileInput;
+
+    @Override
+    public IStatus validate(IProject project, String path) {
+        File file = new File(path);
+        if (!file.exists()) {
+            return new Status(IStatus.ERROR, Activator.PLUGIN_ID, "File not found: " + path); //$NON-NLS-1$
+        }
+        if (!file.isFile()) {
+            return new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Not a file. It's a directory: " + path); //$NON-NLS-1$
+        }
+        int confidence = 0;
+        try {
+            if (!TmfTraceUtils.isText(file)) {
+                return new TraceValidationStatus(confidence, Activator.PLUGIN_ID);
+            }
+        } catch (IOException e) {
+            Activator.getInstance().logError("Error validating file: " + path, e); //$NON-NLS-1$
+            return new Status(IStatus.ERROR, Activator.PLUGIN_ID, "IOException validating file: " + path, e); //$NON-NLS-1$
+        }
+        try (BufferedRandomAccessFile rafile = new BufferedRandomAccessFile(path, "r")) { //$NON-NLS-1$
+            int lineCount = 0;
+            int matches = 0;
+            String line = readNextEventString(rafile);
+            while ((line != null) && (lineCount++ < MAX_LINES)) {
+                try {
+                    ITmfEvent event = ChromiumEvent.parse(this, lineCount, line);
+                    if (event != null) {
+                        matches++;
+                    }
+                } catch (RuntimeException e) {
+                    confidence = Integer.MIN_VALUE;
+                }
+
+                confidence = MAX_CONFIDENCE * matches / lineCount;
+                line = readNextEventString(rafile);
+            }
+        } catch (IOException e) {
+            Activator.getInstance().logError("Error validating file: " + path, e); //$NON-NLS-1$
+            return new Status(IStatus.ERROR, Activator.PLUGIN_ID, "IOException validating file: " + path, e); //$NON-NLS-1$
+        }
+
+        return new TraceValidationStatus(confidence, Activator.PLUGIN_ID);
+    }
+
+    @Override
+    public void initTrace(IResource resource, String path, Class<? extends ITmfEvent> type) throws TmfTraceException {
+        super.initTrace(resource, path, type);
+        String dir = TmfTraceManager.getSupplementaryFileDir(this);
+        fFile = new File(dir + new File(path).getName());
+        if (!fFile.exists()) {
+            Job sortJob = new SortingJob(this, path);
+            sortJob.schedule();
+            try {
+                sortJob.join();
+            } catch (InterruptedException e) {
+                throw new TmfTraceException(e.getMessage(), e);
+            }
+            IStatus result = sortJob.getResult();
+            if (!result.isOK()) {
+                throw new TmfTraceException("Job failed " + result.getMessage());
+            }
+        }
+        try {
+            fFileInput = new BufferedRandomAccessFile(fFile, "r");
+        } catch (IOException e) {
+            throw new TmfTraceException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public synchronized void dispose() {
+        try {
+            fFileInput.close();
+        } catch (IOException e) {
+            Activator.getInstance().logError("Error disposing trace. File: " + getPath(), e); //$NON-NLS-1$
+        }
+        super.dispose();
+
+    }
+
+    @Override
+    public double getLocationRatio(ITmfLocation location) {
+        return (double) getCurrentLocation().getLocationInfo() / fFile.length();
+    }
+
+    @Override
+    public ITmfContext seekEvent(ITmfLocation location) {
+        if (fFile == null) {
+            return INVALID_CONTEXT;
+        }
+        final TmfContext context = new TmfContext(NULL_LOCATION, ITmfContext.UNKNOWN_RANK);
+        if (NULL_LOCATION.equals(location) || fFile == null) {
+            return context;
+        }
+        try {
+            if (location == null) {
+                fFileInput.seek(1);
+            } else if (location.getLocationInfo() instanceof Long) {
+                fFileInput.seek((Long) location.getLocationInfo());
+            }
+            context.setLocation(new TmfLongLocation(fFileInput.getFilePointer()));
+            return context;
+        } catch (final FileNotFoundException e) {
+            Activator.getInstance().logError("Error seeking event. File not found: " + getPath(), e); //$NON-NLS-1$
+            return context;
+        } catch (final IOException e) {
+            Activator.getInstance().logError("Error seeking event. File: " + getPath(), e); //$NON-NLS-1$
+            return context;
+        }
+    }
+
+    @Override
+    public ITmfContext seekEvent(double ratio) {
+        File file = fFile;
+        if (file == null) {
+            return INVALID_CONTEXT;
+        }
+        long filePos = (long) (file.length() * ratio);
+        long estimatedRank = filePos / ESTIMATED_EVENT_SIZE;
+        return seekEvent(new TmfLongLocation(estimatedRank));
+    }
+
+    @Override
+    public ITmfEvent parseEvent(ITmfContext context) {
+        @Nullable
+        ITmfLocation location = context.getLocation();
+        if (location instanceof TmfLongLocation) {
+            TmfLongLocation tmfLongLocation = (TmfLongLocation) location;
+            Long locationInfo = tmfLongLocation.getLocationInfo();
+            if (location.equals(NULL_LOCATION)) {
+                locationInfo = 0L;
+            }
+            if (locationInfo != null) {
+                try {
+                    if (!locationInfo.equals(fFileInput.getFilePointer())) {
+                        fFileInput.seek(locationInfo);
+                    }
+                    return ChromiumEvent.parse(this, context.getRank(), readNextEventString(fFileInput));
+                } catch (IOException e) {
+                    Activator.getInstance().logError("Error parsing event", e); //$NON-NLS-1$
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public ITmfLocation getCurrentLocation() {
+        long temp = -1;
+        try {
+            temp = fFileInput.getFilePointer();
+        } catch (IOException e) {
+        }
+        return new TmfLongLocation(temp);
+    }
+
+    @Override
+    public @NonNull Map<@NonNull String, @NonNull String> getProperties() {
+        return Collections.singletonMap("Type", "Trace-Event"); //$NON-NLS-1$ //$NON-NLS-2$
+    }
+
+    @Override
+    public ITmfLocation restoreLocation(ByteBuffer bufferIn) {
+        return new TmfLongLocation(bufferIn);
+    }
+
+    @Override
+    public int getCheckpointSize() {
+        return 10000;
+    }
+
+    private static @Nullable String readNextEventString(RandomAccessFile parser) throws IOException {
+        StringBuffer sb = new StringBuffer();
+        char elem = (char) parser.read();
+        if (elem == ']') {
+            return null;
+        }
+        while (elem != '}' && elem != ']' && elem != (char) -1) {
+            elem = (char) parser.read();
+            sb.append(elem);
+        }
+        return (elem == '}') ? sb.toString() : null;
+    }
+
+}
