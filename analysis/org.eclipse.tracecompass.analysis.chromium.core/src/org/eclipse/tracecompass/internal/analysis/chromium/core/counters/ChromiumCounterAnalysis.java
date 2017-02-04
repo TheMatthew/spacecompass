56075df2dd97cf93ad45503bef32219850fc2a5d
Matthew Khouzam <matthew.khouzam@gmail.com>
2017-02-03 19:43:21 -0500

    add counter analysis
    Signed-off-by: Matthew Khouzam <matthew.khouzam@gmail.com>

---
+++ analysis/org.eclipse.tracecompass.analysis.chromium.core/src/org/eclipse/tracecompass/internal/analysis/chromium/core/counters/ChromiumCounterAnalysis.java
@@ -0,0 +1,17 @@
+package org.eclipse.tracecompass.internal.analysis.chromium.core.counters;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.tracecompass.common.core.NonNullUtils;
+import org.eclipse.tracecompass.tmf.core.statesystem.ITmfStateProvider;
+import org.eclipse.tracecompass.tmf.core.statesystem.TmfStateSystemAnalysisModule;
+import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
+
+public class ChromiumCounterAnalysis extends TmfStateSystemAnalysisModule {
+
+    @Override
+    protected @NonNull ITmfStateProvider createStateProvider() {
+        ITmfTrace trace = getTrace();
+        return new ChromiumCounterProvider(NonNullUtils.checkNotNull(trace));
+    }
+
+}
