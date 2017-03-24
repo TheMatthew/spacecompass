7b01ddbd2e0f3e71a45c6fb7f9601bcf32477b17
Matthew Khouzam <matthew.khouzam@gmail.com>
2017-03-23 23:36:10 -0400

    Add support for flow events.
    This populates the callstack with flow events and adds arrows
    which show the causality. So basically full wait chains can be
    re-derived.
    
    Signed-off-by: Matthew Khouzam <matthew.khouzam@gmail.com>

--- analysis/org.eclipse.tracecompass.analysis.chromium.core/src/org/eclipse/tracecompass/internal/analysis/chromium/core/event/TraceEventLookup.java
+++ analysis/org.eclipse.tracecompass.analysis.chromium.core/src/org/eclipse/tracecompass/internal/analysis/chromium/core/event/TraceEventLookup.java
@@ -25,7 +25,7 @@ public class TraceEventLookup {
         TYPES['B'] = new TmfEventType("Entry", null); //$NON-NLS-1$
         TYPES['E'] = new TmfEventType("Exit", null); //$NON-NLS-1$
         TYPES['X'] = new TmfEventType("Complete", null); //$NON-NLS-1$
-        TYPES['I'] = new TmfEventType("Instant", null); //$NON-NLS-1$
+        TYPES['I'] = TYPES['i'] = new TmfEventType("Instant", null); //$NON-NLS-1$
         TYPES['b'] = new TmfEventType("Nested Begin", null); //$NON-NLS-1$
         TYPES['n'] = new TmfEventType("Nested Info", null); //$NON-NLS-1$
         TYPES['e'] = new TmfEventType("Nested End", null); //$NON-NLS-1$
@@ -33,6 +33,9 @@ public class TraceEventLookup {
         TYPES['D'] = new TmfEventType("Object Destroyed", null); //$NON-NLS-1$
         TYPES['C'] = new TmfEventType("Counter", null); //$NON-NLS-1$
         TYPES['R'] = new TmfEventType("Mark", null); //$NON-NLS-1$
+        TYPES['s'] = new TmfEventType("Flow Start", null); //$NON-NLS-1$
+        TYPES['t'] = new TmfEventType("Flow Step", null); //$NON-NLS-1$
+        TYPES['f'] = new TmfEventType("Flow End", null); //$NON-NLS-1$
     }
 
     /**
